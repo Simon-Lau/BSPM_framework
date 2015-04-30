cfcafc7a1cdd634e2e44985dea92eaa29fa4f80c
刘平 <tonylp@126.com>
2015-04-30 23:39:03 +0800

    Db connect
    modify of load.php to make it more efficient
    add db.php include connect, query close and so on. divide into read and
    write permission, still have some problem of zkname

--- config/cfg_db_user.php
+++ config/cfg_db_user.php
@@ -11,14 +11,16 @@ return array(
     'charset' => 'utf8',
     'write' => array(
         'default' => '127.0.0.1',
-        'dbhost' => 'www.hit.edu.cn',  //use zkname and need to decode it or you can use just a ip address
+        //'dbhost' => 'www.hit.edu.cn',  //use zkname and need to decode it or you can use just a ip address
+        'dbhost' => '127.0.0.1',
         'username' => 'webframe',
         'password' => 'webframe',
         'port' => '3303',
     ),
     'read' => array(
         'default' => '127.0.0.1',
-        'dbhost' => 'www.hit.edu.cn',
+        // 'dbhost' => 'www.hit.edu.cn',
+        'dbhost' => '127.0.0.1',
         'username' => 'webframe_r',
         'password' => 'webframe_read',
         'port' => '3302',
--- lib/Dd.php
+++ lib/Dd.php
@@ -7,16 +7,123 @@
 * April 30, 2015
 **/
 
- class Web_Db{
+class Web_db{
+
  	protected $_config = array();
- 	protected $rdb = array();
- 	protected $wdb = array();
+ 	protected $_rdb = null;   //read db
+ 	protected $_wdb = null;   //write db
+ 	protected $_queryType = false;  // false is for read query and true for write query
 
  	public function __construct(array $config){
+ 		if(!isset($config['dbname']) || !isset($config['write'])){
+ 			throw new Web_Exception("config array error : dbname/write required");
+ 		}
  		$this->_config = $config;
  	}
 
  	public function getConfig(){
  		return $this->_config;
  	}
- }
+
+    public function closeDb(){
+    	$this->_rdb = $this->_wdb = null;
+    }
+
+    public function setQueryType($type){
+		$this->_queryType = $type;
+	}
+
+	public function query(){
+		return $this->_query($sql);
+	}
+
+	public function exec($sql){
+		return $this->_exec($sql);
+	}
+
+	public function listTables(){
+		$stmt = $this->query("SHOW TABLES");
+		return $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+	}
+
+ 	protected function _connect($config){
+ 		if(!isset($config['dbhost']) || !isset($config['username']) || !isset($config['password'])){
+ 			throw new Web_Exception("config array error dbhost/username/password required")
+ 		}
+ 		// still some problem with zkname
+ 		if(strpos($config['dbhost'],'www.') > 0){
+ 		// 	require_once('/zk_agent/names/nameapi.php');
+			// $zk_host = new ZkHost;
+
+			// $ret = getHostByKey($config['dbhost'], $zk_host);
+			// if(0 == $ret && !empty($zk_host->ip) && !empty($zk_host->port))
+			// {
+			// 	$config['dbhost'] = $zk_host->ip;
+			// 	$config['port'] = $zk_host->port;
+			// }  
+		}
+
+		$dsn = 'mysql:'.'host='.$config['dbhost'].';dbname='.$this->_config['dbname'];
+		if(isset($config['port'])){
+			$dsn .= ';port=' . $config['port'];
+		}
+		$driver_options = array();
+		if(isset($this->_config['charset'])){
+			$driver_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET character_set_connection=".$this->_config['charset'].", character_set_results=".$this->_config['charset'].", character_set_client=binary,sql_mode=''";
+		}
+		try{
+			$db = new PDO($dsn,$config['username'],$config['password'],$driver_options);
+		}catch(PDOException $e){
+			throw new Web_Exception($e->getMessage(),$e->getCode());
+		}
+		return $db;
+ 	}
+
+ 	protected function _rconnect(){
+ 		if($this->_rdb){
+ 			return $this->_rdb;
+ 		}
+ 		if(!isset($this->_config['read'])){
+ 			$this->_rdb = $this->_wconnect();
+ 		}else{
+ 			$this->_rdb = $this->_connect($this->_config['read']);
+ 		}
+ 		return $this->_rdb;
+ 	}
+
+ 	protected function _wconnect(){
+ 		if($this->_wdb){
+ 			return $this->_wdb;
+ 		}
+ 		$this->_wdb = $this->_connect($this->_config['write']);
+ 		return $this->_wdb;
+ 	}
+
+ 	protected function _query($sql){
+ 		if($this->_queryType){
+	 		$this->_wconnect();
+	 		if(!$stmt = $this->_wdb->query($sql)){
+	 			$tmparr = $this->_wdb->errorInfo();
+	 			throw new Web_Exception($tmparr[2].'), SQL:'.$sql, $tmparr[1]);
+	 		}
+ 		}else{
+ 			$this->_rconnect();
+	 		if(!$stmt = $this->_rdb->query($sql)){
+	 			$tmparr = $this->_rdb->errorInfo();
+	 			throw new Web_Exception($tmparr[2].'), SQL:'.$sql, $tmparr[1]);
+	 		}
+ 		}
+ 		return $stmt;
+ 	}
+
+ 	protected function _exec($sql){
+ 		$this->_wconnect();
+ 		if($rowCount = $this->_wdb->exec($sql) == false){
+			$tmparr = $this->_wdb->errorInfo();
+	 		throw new Web_Exception($tmparr[2].', SQL:'.$sql, $tmparr[1]);
+ 		}
+
+ 		return $rowCount;
+ 	} 	
+ 	
+}
